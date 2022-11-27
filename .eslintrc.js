b24bb5b56798be632e963160e0f04a0212886309
Yoon-Hae-Min <yunhatmi@naver.com>
2022-11-27 15:34:45 +0900

    chore: 프로젝트 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  ignorePatterns: ['*.js', '*.json'],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {}
+};
