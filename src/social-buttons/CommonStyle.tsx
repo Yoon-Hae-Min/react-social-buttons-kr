8f6b6e1186f7c22463dc9985adabb2b6f01f1aad
Yoon-Hae-Min <yunhatmi@naver.com>
2022-12-03 13:28:16 +0900

    refactor: 공통 스타일의 타입 추가

--- src/social-buttons/CommonStyle.tsx
+++ src/social-buttons/CommonStyle.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { SYMBOL_HEIGHT, SYMBOL_WIDTH } from '../constants/size';
 import { SocialLoginButton } from '../types/common';
 
 const defaultWidth = {
@@ -13,28 +14,35 @@ const defaultHeight = {
   circle: '3.5rem',
 };
 
-export const SocialButton = styled.button<SocialLoginButton>`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: ${(props) => props.width ?? defaultWidth[props.shape ?? 'standard']};
-  height: ${(props) => props.height ?? defaultHeight[props.shape ?? 'standard']};
-  padding: 0.5rem;
-  cursor: pointer;
-  border-radius: ${(props) => (props.isRound ?? true ? '0.5rem' : 'none')};
-  ${(props) => props.shape === 'circle' && 'border-radius: 50%;'}
-  border: none;
-  outline: none;
-`;
-
+export const SocialButton = <T extends SocialLoginButton>() =>
+  styled.button<T>`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: ${(props) => props.width ?? defaultWidth[props.shape ?? 'standard']};
+    height: ${(props) => props.height ?? defaultHeight[props.shape ?? 'standard']};
+    padding: 0.5rem;
+    cursor: pointer;
+    border-radius: ${(props) => (props.isRound ?? true ? '0.5rem' : 'none')};
+    ${(props) => props.shape === 'circle' && 'border-radius: 50%;'}
+    border: none;
+    outline: none;
+  `;
 const alignCss = {
   start: 'margin: auto;',
   center: '',
 };
 
-export const SocialText = styled.span<SocialLoginButton>`
-  ${(props) => alignCss[props.align ?? 'start']};
-  font-size: 1rem;
-  text-align: center;
-  color: #191919;
+export const SocialText = <T extends SocialLoginButton>() =>
+  styled.span<T>`
+    ${(props) => alignCss[props.align ?? 'start']};
+    font-size: 1rem;
+    text-align: center;
+    color: #191919;
+  `;
+
+export const Symbol = styled.div`
+  width: ${SYMBOL_WIDTH};
+  height: ${SYMBOL_HEIGHT};
+  margin: 0rem 0.6rem 0rem 0.6rem;
 `;
